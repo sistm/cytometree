881570ca8ba237df5d77ac421d85d746a594099f
Boris <boris.hejblum.ensai@gmail.com>
2017-05-23 14:44:37 +0200

    adding readme and citation ref + updating date and version
    CRAN ready

--- R/cytometree-package.R
+++ R/cytometree-package.R
@@ -2,21 +2,21 @@
 #'
 #'The algorithm is based on the construction of a binary tree, 
 #'the nodes of which are subpopulations of cells. At each node, 
-#'observed cells and markers are modeled by both a familly of normal
-#'and a familly of normal mixtures distributions. 
-#'Spliting is done according to a normalized difference of AIC between 
+#'observed cells and markers are modeled by both a family of normal
+#'and a family of normal mixtures distributions. 
+#'Splitting is done according to a normalized difference of AIC between 
 #'the two families.
 #'Given the unsupervised nature of the binary tree, some of the available
 #'markers may not be used to find the different cell populations present in 
 #'a given sample. To recover a complete annotation, we defined, as a post 
 #'processing procedure, an annotation method which allows the user to 
-#'distinguish two or three expression levels per marker
+#'distinguish two or three expression levels per marker.
 #'
 #'\tabular{ll}{
 #'Package: \tab cytometree\cr
 #'Type: \tab Package\cr
-#'Version: \tab 1.0\cr
-#'Date: \tab 2017-05-17\cr
+#'Version: \tab 1.0.0\cr
+#'Date: \tab 2017-05-23\cr
 #'License:\tab \href{http://www.gnu.org/licenses/lgpl.txt}{LGPL-3}\cr
 #'}
 #'The main function in this package is \code{\link{CytomeTree}}.
@@ -24,7 +24,7 @@
 #'@author Chariff Alkhassim, Boris P. Hejblum
 #'--- Maintainer: Chariff Alkhassim
 #'
-#'@references TODO
+#'@references D. Commenges, C Alkhassim, B.P. Hejblum, R. Thiébaut. A Binary tree algorithm for automatic gating in cytometry analysis. Sumitted, 2017.
 #'
 #'@docType package
 #'@name cytometree-package
