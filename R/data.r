a7134afaefe6f65ab18756cfdbf79b15a6f045db
chariff <chariff.alkhassim@gmail.com>
2017-07-13 18:04:25 +0200

    Updating the vignette with a new example.

---
+++ R/data.r
@@ -0,0 +1,27 @@
+#' Diffuse large B-cell lymphoma data set from the flowCAP-I challenge.
+#'
+#'@docType data
+#'
+#'@keywords data
+#'
+#'@name DLBCL
+#'
+#'@usage data(DLBCL)
+#'
+#'@format A data frame with 5524 cell events and 3 markers.
+#'
+#'@source \url{http://flowcap.flowsite.org/}
+#'
+#' HIPC T cell data set from HIPC program, patient 12828. The data was analysed and gated by Stanford.
+#'
+#'@docType data
+#'
+#'@keywords data
+#'
+#'@name HIPC
+#'
+#'@usage data(HIPC)
+#'
+#'@format A data frame with 33992 cell events and 6 markers.
+#'
+#'@source \url{https://www.immuneprofiling.org/}
