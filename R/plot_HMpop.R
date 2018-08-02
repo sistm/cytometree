9612971a68a8913531f6155600c0c6c927564345
Anthony DEVAUX <anthony.devaux35@gmail.com>
2018-08-02 15:10:13 +0200

    add plot_HMpop function

---
+++ R/plot_HMpop.R
@@ -0,0 +1,55 @@
+#' Plot a heatmap representing the median of the expression for each population and marker using CytomeTree
+#' 
+#' @param TreeObj An object of class CytomeTree
+#' 
+#' @param AnnotObj An object of class Annotation
+#' 
+#' @author Anthony Devaux, Boris Hejblum
+#' 
+#' @import robustbase pheatmap RColorBrewer
+#' 
+#' @export plot_HMpop
+#' 
+#' @examples 
+#' 
+#' # Run CytomeTree
+#' data(DLBCL)
+#' cellevents <- DLBCL[,c("FL1", "FL2", "FL4")]
+#' Tree <- CytomeTree(cellevents, minleaf = 1, t=.1)
+#' Annot <- Annotation(Tree,plot=FALSE)
+#' 
+#' # Plot the cell count
+#' plot_HMpop(Tree,Annot)
+
+
+plot_HMpop <- function(TreeObj, AnnotObj) {
+  
+  if (class(TreeObj)!="CytomeTree") {
+    
+    stop("TreeObj must be class of CytomeTree")
+    
+  }
+  
+  if (class(AnnotObj)!="Annotation") {
+    
+    stop("AnnotObj must be class of Annotation")
+    
+  }
+  
+  nbClust <- max(AnnotObj$labels)
+  data <- matrix(ncol = ncol(TreeObj$M))
+  
+  for (numclust in 1:nbClust) {
+    
+    data <- rbind(data,robustbase::colMedians(as.matrix(TreeObj$M[which(AnnotObj$labels==numclust),])))
+    
+  }
+  
+  data <- t(data[-1,])
+  colnames(data) <- c(1:nbClust)
+  
+  pheatmap::pheatmap(data, scale = "row",
+           color = colorRampPalette(RColorBrewer::brewer.pal(n = 10, name =
+                                                     "RdYlGn"))(100), cluster_rows = FALSE)
+
+}
