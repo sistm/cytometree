720b3307e4fc37fc01c42ab13ed1ed412935ac79
anthonydevaux <>
2019-04-02 18:50:25 +0200

    Create CytGridsearch.R

---
+++ R/CytGridsearch.R
@@ -0,0 +1,21 @@
+x <- c(rnorm(100,4,2), rnorm(200, -25, 0.1))
+resu_aic_1_gauss <- aic_1_gauss(xx)
+resu_aic_2_gauss <- aic_2_gauss(xx, init = c(0.5, 0, 0, 2, 2))
+
+
+CytGridsearch <- function(x, resu_aic_1_gauss, resu_aic_2_gauss, iter_max = 15,
+                           ntry = 30){
+  
+  list_aic2 <- lapply(1:ntry, function(i){
+    
+    init_p <-0.5
+    init_mu1 <- rnorm(1, resu_aic_1_gauss$mu, resu_aic_1_gauss$var_mu)
+    init_mu2 <- rnorm(1, resu_aic_1_gauss$mu, resu_aic_1_gauss$var_mu)
+    init_sigma1 <- rnorm(1, resu_aic_1_gauss$sigma, resu_aic_1_gauss$var_sigma)
+    init_sigma2 <- rnorm(1, resu_aic_1_gauss$mu, resu_aic_1_gauss$var_mu)
+    return(aic_2_gauss(x, init = c(init_p, 
+                                   init_mu1, init_mu2,
+                                   init_sigma1, init_sigma2), maxit = iter_max))
+  })
+  best_aic2 <- list_aic2[[which.min(sapply(list_aic2, "[[", "AIC"))]]
+}
