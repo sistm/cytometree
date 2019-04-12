0a2a02137a16d82acbeb58f59e83fca21b7f6ee8
anthonydevaux <38243147+anthonydevaux@users.noreply.github.com>
2019-04-12 17:16:29 +0200

    maj testthat

--- tests/testthat/test-aic_2_gauss.R
+++ tests/testthat/test-aic_2_gauss.R
@@ -3,7 +3,7 @@ context("test-aic_2_gauss")
 x <- c(rnorm(100,m=4,sd=2), rnorm(200,m=-25,sd=0.1))
 res <- aic_2_gauss(x, init = "kmeans", maxit = 100)
 
-test_that("loglikelihood is correct", {
+test_that("AIC is correct", {
   expect_equal(res$AIC, 
                -2*sum(
                  log(res$p * exp(-(x-res$mu1)^2/(2*res$s1^2))/(res$s1 * sqrt(2 * pi)) + 
--- tests/testthat/test-aic_3_gauss.R
+++ tests/testthat/test-aic_3_gauss.R
@@ -4,7 +4,7 @@ x <- c(rnorm(100,4,2), rnorm(600, -25, 0.1), rnorm(300, 30, 4))
 res <- aic_3_gauss(x, init = "kmeans", maxit = 100)
 #res <- aic_3_gauss(x, init = c(0.3, 0.3, 0, -10, 10, 2, 2, 2), maxit = 100)
 
-test_that("loglikelihood is correct", {
+test_that("AIC is correct", {
   expect_equal(res$AIC,
                -2*sum(
                  log(res$p1 * exp(-(x-res$mu1)^2/(2*res$s1^2))/(res$s1 * sqrt(2 * pi)) + 
