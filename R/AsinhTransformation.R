7f8c77dbc81a2e7fb644e6deddae942225bb8d41
anthonydevaux <>
2019-07-20 16:49:16 +0200

    Update cytoftree function

--- R/AsinhTransformation.R
+++ R/AsinhTransformation.R
@@ -1,3 +1,7 @@
+#' Data transformation using asinh
+#'
+#' @keywords internal
+
 AsinhTransformation <- function(M, num_col, a = 0, b = 0.2, c = 0){
   
   M[,num_col] <- asinh((a+b*M[,num_col])+c)
--- R/CytofTree.R
+++ R/CytofTree.R
@@ -1,6 +1,6 @@
-#' Binary tree algorithm for cytometry data analysis.
+#' Binary tree algorithm for mass cytometry data analysis.
 #'
-#'@param M A matrix of size n x p containing cytometry measures
+#'@param M A matrix of size n x p containing mass cytometry measures
 #'of n cells on p markers.
 #'
 #'@param minleaf An integer indicating the minimum number of cells
@@ -19,33 +19,58 @@
 #'and forcing the split at every node. Default is \code{NULL}, in which case
 #'the clustering algorithm is unsupervised.
 #'
+#'@param transformation A string indicating the transformation used among \code{asinh} 
+#'\code{log10} and \code{none}. Default is \code{asinh} transformation.
+#'
+#'@param num_col An integer vector of index indicating the columns to be transform. 
+#'Default is \code{1:ncol(M)} to transform all the data. 
+#'
 #'@return An object of class 'cytomeTree' providing a partitioning
 #'of the set of n cells.
 #'\itemize{
 #'\item{\code{annotation}}{ A \code{data.frame} containing the annotation of each 
 #'cell population underlying the tree pattern.}
 #'\item{\code{labels}}{ The partitioning of the set of n cells.}
-#'\item{\code{M}}{ The input matrix.}
+#'\item{\code{M}}{ The transformed matrix of mass cytometry.}
 #'\item{\code{mark_tree}}{ A two level list containing markers used
 #'for node splitting.}
+#'\item{\code{transformation}}{ Transformation used}
+#'\item{\code{num_col}}{ Indexes of columns transformed}
 #' }
 #'
-#'@details The algorithm is based on the construction of a binary tree,
+#'@details Previously, dhe data can be transform using different kind of transformations. 
+#'The algorithm is based on the construction of a binary tree,
 #'the nodes of which are subpopulations of cells. At each node,
 #'observed cells and markers are modeled by both a family of normal
 #'distributions and a family of bi-modal normal mixture distributions.
 #'Splitting is done according to a normalized difference of AIC between
 #'the two families.
-#'@author Chariff Alkhassim, Boris Hejblum
+#'@author Anthony Devaux, Boris Hejblum
 #'
 #'@export
 #'
 #'@examples
-#'head(DLBCL)
+#'data(IMdata)
+#'
+#'# dimension of data
+#'dim(IMdata)
+#'
+#'# Don't transform Time et Cell_length column
+#'num_col <- 3:ncol(M)
+#'
+#'# Build Cytoftree binary tree
+#'tree <- CytofTree(M = IMdata, minleaf = 1, t = 0.1, transformation = "asinh", num_col = num_col)
+#'
+#'# Annotation
+#'annot <- Annotation(tree, plot = FALSE, K2markers = colnames(IMdata))
+#'
+#'# Provide subpopulations
+#'annot$combinations
+#'
 
-CytofTree <- function(M, minleaf = 1, t = .1, verbose = TRUE, 
-                      force_first_markers = NULL, transformation = c("asinh", "none"), 
-                      num_col = NULL){
+CytofTree <- function(M, minleaf = 1, t = .1, verbose = TRUE, force_first_markers = NULL, 
+                      transformation = c("asinh", "log10", "none"), 
+                      num_col = 1:ncol(M)){
   if(class(M) == "data.frame"){
     M <- as.matrix(M)
   }
@@ -78,16 +103,29 @@ CytofTree <- function(M, minleaf = 1, t = .1, verbose = TRUE,
     }
   }
   
-  if(any(!transformation%in%c("asinh", "none"))){
-    stop("The only allowed values for transformation are 'asinh' or 'none'")
+  if(any(!transformation%in%c("asinh", "log10", "none"))){
+    stop("The only allowed values for transformation are 'asinh', 'log10' or 'none'")
   }
   
-  if(any(transformation%in%c("asinh"))){
+  if(length(transformation)>1){
+    transformation <- transformation[1]
+    warning("Default 'asinh' transformation used")
+  }
+  
+  if(transformation!="none"){
     if(any(!(num_col%in%1:ncol(M)))){
       stop(paste0("'num_col' should contain columns indices from 1 to ", ncol(M)))
-    }else{
-      M <- AsinhTransformation(M, num_col)
     }
+  }else{
+    num_col <- NULL
+  }
+  
+  if(transformation=="asinh"){
+    M <- AsinhTransformation(M, num_col)
+  }
+  
+  if(transformation=="log10"){
+    M <- Log10Transformation(M, num_col)
   }
   
   BT <- Cytof_BinaryTree(M, floor(minleaf), t, verbose, force_first_markers)
