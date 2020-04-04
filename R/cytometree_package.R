a2df847bfd2e20474d39315040a0ea5c52d8a04f
borishejblum <boris.hejblum@u-bordeaux.fr>
2020-04-04 20:15:11 +0200

    adding a logo

--- R/cytometree_package.R
+++ R/cytometree_package.R
@@ -1,5 +1,15 @@
-#'Binary tree algorithm for cytometry data analysis.
+#'@useDynLib cytometree, .registration = TRUE
+#'@importFrom Rcpp sourceCpp evalCpp
 #'
+#'@details 
+#'The main function in this package is \code{\link{CytomeTree}}.
+#'\tabular{ll}{
+#'Package: \tab cytometree\cr
+#'Type: \tab Package\cr
+#'Version: \tab 2.0.2\cr
+#'Date: \tab 2019-12-04\cr
+#'License:\tab \href{http://www.gnu.org/licenses/lgpl.txt}{LGPL-3}\cr
+#'}
 #'The algorithm is based on the construction of a binary tree, 
 #'the nodes of which are subpopulations of cells. At each node, 
 #'observed cells and markers are modeled by both a family of normal
@@ -12,27 +22,8 @@
 #'processing procedure, an annotation method which allows the user to 
 #'distinguish two or three expression levels per marker.
 #'
-#'\tabular{ll}{
-#'Package: \tab cytometree\cr
-#'Type: \tab Package\cr
-#'Version: \tab 2.0.2\cr
-#'Date: \tab 2019-12-04\cr
-#'License:\tab \href{http://www.gnu.org/licenses/lgpl.txt}{LGPL-3}\cr
-#'}
-#'The main function in this package is \code{\link{CytomeTree}}.
-#'
-#'@author Chariff Alkhassim, Boris Hejblum, Anthony Devaux, Van Hung Huynh Tran
-#'--- Maintainer: Boris Hejblum
-#'
 #'@references Commenges D, Alkhassim C, Gottardo R, Hejblum BP, Thiébaut R (2018). 
 #'cytometree: a binary tree algorithm for automatic gating in cytometry analysis. 
 #'Cytometry Part A, 93(11):1132-1140. <doi: 10.1002/cyto.a.23601>
 #'
-#'@docType package
-#'@name cytometree-package
-#'@aliases cytometree
-#'
-#'@useDynLib cytometree, .registration = TRUE
-#'@importFrom Rcpp evalCpp
-#'
-NULL
+"_PACKAGE"
