3db6bfb2e4fcf69672bf3c2ecab739e8abbb3b44
borishejblum <boris.hejblum@u-bordeaux.fr>
2022-02-09 12:02:49 +0100

    fixing CytEM test

--- tests/testthat/test_CytEM.R
+++ tests/testthat/test_CytEM.R
@@ -3,8 +3,8 @@ data(DLBCL)
 cellevents <- DLBCL[, c("FL1", "FL2", "FL4")]
 
 context("test CytEM")
-
+myminleaf <- 100
 test_that("minleaf works as expected", {
-  expect_gte(length(CytEM(M = as.matrix(cellevents), indices = 1:nrow(cellevents), minleaf = 100, level = 1, t = 0.1, force_marker = NULL)[["child"]][["L"]]), minleaf)
-  expect_gte(length(CytEM(M = as.matrix(cellevents), indices = 1:nrow(cellevents), minleaf = 100, level = 1, t = 0.1, force_marker = NULL)[["child"]][["R"]]), minleaf)
+  expect_gte(length(CytEM(M = as.matrix(cellevents), indices = 1:nrow(cellevents), minleaf = myminleaf, level = 1, t = 0.1, force_marker = NULL)[["child"]][["L"]]), myminleaf)
+  expect_gte(length(CytEM(M = as.matrix(cellevents), indices = 1:nrow(cellevents), minleaf = myminleaf, level = 1, t = 0.1, force_marker = NULL)[["child"]][["R"]]), myminleaf)
 })
